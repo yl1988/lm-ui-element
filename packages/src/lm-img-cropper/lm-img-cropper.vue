a5fad44725bbc21a0aaa4a5d6922dfd971466d64
Crazy <744890753@qq.com>
2021-01-06 19:41:11 +0800

    拉取合并

--- packages/src/lm-img-cropper/lm-img-cropper.vue
+++ packages/src/lm-img-cropper/lm-img-cropper.vue
@@ -23,21 +23,21 @@
                     <div class="operation-box">
                         <div class="cropper" :style="{width:autoCropWidth+60+'px',height:priveImgHeight+60+'px'}">
                             <vueCropper
-                                ref="cropper"
-                                :img="imgSrc"
-                                :outputSize="outputSize"
-                                :outputType="outputType"
-                                :info="info"
-                                :full="full"
-                                :canMove="canMove"
-                                :canMoveBox="canMoveBox"
-                                :original="original"
-                                :autoCrop="autoCrop"
-                                :autoCropWidth="priveImgWidth"
-                                :autoCropHeight="priveImgHeight"
-                                :fixedBox="fixedBox"
-                                @realTime="realTime"
-                                @imgLoad="imgLoad"
+                                    ref="cropper"
+                                    :img="imgSrc"
+                                    :outputSize="outputSize"
+                                    :outputType="outputType"
+                                    :info="info"
+                                    :full="full"
+                                    :canMove="canMove"
+                                    :canMoveBox="canMoveBox"
+                                    :original="original"
+                                    :autoCrop="autoCrop"
+                                    :autoCropWidth="priveImgWidth"
+                                    :autoCropHeight="priveImgHeight"
+                                    :fixedBox="fixedBox"
+                                    @realTime="realTime"
+                                    @imgLoad="imgLoad"
                             ></vueCropper>
                             <div class="rowCenter loadingBox" v-if="showLoading">
                                 <i class="el-icon-loading font40 gray666"></i>
@@ -78,156 +78,158 @@
 </template>
 
 <script>
-
-import LmDialog from '../lm-dialog/lm-dialog'
-export default {
+  import Vue from 'vue'
+  import LmDialog from '../lm-dialog/lm-dialog'
+  import VueCropper  from 'vue-cropper'
+  Vue.use(VueCropper)
+  export default {
     name: 'LmImgCropper',
     components:{
-        LmDialog
+      LmDialog
     },
     props:{
-        imgSrc:[String,Blob],// 裁剪图片的地址
-        info: {
-            type:Boolean,
-            default:true
-        }, // 显示裁剪框的大小信息
-        outputSize: {
-            type:Number,
-            default:1
-        }, // 剪切后的图片质量（0.1-1）
-        full: {
-            type:Boolean,
-            default:true
-        }, // 输出原图比例截图 props名full
-        outputType: String, // 裁剪生成额图片的格式
-        canMove: {
-            type:Boolean,
-            default:true
-        },  // 能否拖动图片
-        original: Boolean,  // 上传图片是否显示原始宽高
-        canMoveBox: {
-            type:Boolean,
-            default:true
-        },  // 能否拖动截图框
-        autoCrop: {
-            type:Boolean,
-            default:true
-        }, // 是否默认生成截图框
-        autoCropWidth: {
-            type:Number,
-            default:200
-        },//裁剪框宽度
-        autoCropHeight: {
-            type:Number,
-            default:260
-        },//裁剪框宽度
-        fixedBox: Boolean, // 截图框固定大小
-        fileSize:Number,//文件尺寸
-        exampleImg:{
-            type:String,
-            default:require('./exampleImg.svg')
-        },//示例图片路径
-        maxSize:{
-            type:[Number,String],
-            default:200
-        },//允许的最大尺寸
-        title:{
-            type:String,
-            default:'图片裁剪'
-        },//标题
-        width:{
-            type:[Number,String],
-            default:850
-        },//宽度
-        previewText:{
-            type:String,
-            default:'预览'
-        },//预览文字
-        exampleText:{
-            type:String,
-            default:'示例'
-        },//是例文字
-        completeBtnText:{
-            type:String,
-            default:'裁剪完成'
-        },//裁剪完成按钮
-        cancelBtnText:{
-            type:String,
-            default:'不裁剪'
-        },//裁剪完成按钮
+      imgSrc:[String,Blob],// 裁剪图片的地址
+      info: {
+        type:Boolean,
+        default:true
+      }, // 显示裁剪框的大小信息
+      outputSize: {
+        type:Number,
+        default:1
+      }, // 剪切后的图片质量（0.1-1）
+      full: {
+        type:Boolean,
+        default:true
+      }, // 输出原图比例截图 props名full
+      outputType: String, // 裁剪生成额图片的格式
+      canMove: {
+        type:Boolean,
+        default:true
+      },  // 能否拖动图片
+      original: Boolean,  // 上传图片是否显示原始宽高
+      canMoveBox: {
+        type:Boolean,
+        default:true
+      },  // 能否拖动截图框
+      autoCrop: {
+        type:Boolean,
+        default:true
+      }, // 是否默认生成截图框
+      autoCropWidth: {
+        type:Number,
+        default:200
+      },//裁剪框宽度
+      autoCropHeight: {
+        type:Number,
+        default:260
+      },//裁剪框宽度
+      fixedBox: Boolean, // 截图框固定大小
+      fileSize:Number,//文件尺寸
+      exampleImg:{
+        type:String,
+        default:require('./exampleImg.svg')
+      },//示例图片路径
+      maxSize:{
+        type:[Number,String],
+        default:200
+      },//允许的最大尺寸
+      title:{
+        type:String,
+        default:'图片裁剪'
+      },//标题
+      width:{
+        type:[Number,String],
+        default:850
+      },//宽度
+      previewText:{
+        type:String,
+        default:'预览'
+      },//预览文字
+      exampleText:{
+        type:String,
+        default:'示例'
+      },//是例文字
+      completeBtnText:{
+        type:String,
+        default:'裁剪完成'
+      },//裁剪完成按钮
+      cancelBtnText:{
+        type:String,
+        default:'不裁剪'
+      },//裁剪完成按钮
     },
     data() {
-        return {
-            previews: {},//图片预览数据
-            showLoading:true,//是否显示加载
-            priveImgHeight:0,//预览图高度
-            priveImgWidth:0,//预览图宽度
-        }
+      return {
+        previews: {},//图片预览数据
+        showLoading:true,//是否显示加载
+        priveImgHeight:0,//预览图高度
+        priveImgWidth:0,//预览图宽度
+      }
     },
     created(){
-        //console.log(this.imgSrc)
-        this.priveImgHeight=this.autoCropWidth*374/264
-        this.priveImgWidth=this.autoCropWidth
+      //console.log(this.imgSrc)
+      this.priveImgHeight=this.autoCropWidth*374/264
+      this.priveImgWidth=this.autoCropWidth
     },
     methods: {
-        // 放大/缩小
-        changeScale(num) {
-            num = num || 1
-            this.$refs.cropper.changeScale(num)
-        },
-        // 旋转
-        rotate(type) {
-            //type 0 左旋 1右旋
-            type ? this.$refs.cropper.rotateRight() : this.$refs.cropper.rotateLeft()
-        },
-        // 实时预览函数
-        realTime(data) {
-            // //console.log('realTime')
-            // //console.log(data)
-            this.previews = data
-        },
-        //绑定监听函数
-        openDialog(){
-            this.showLoading=true
-            // //console.log(this.$refs.customDialog)
-            let {overy,closeIcon}=this.$refs.customDialog.$refs
-            // //console.log(overy)
-            // //console.log(closeIcon)
-            return new Promise((resolve,reject)=>{
-                this.$refs.complete.addEventListener('click',async ()=>{
-                    let file=await this.getCropBlob()
-                    this.$emit('cropperSuccess',file)
-                    resolve(file)
-                },true)
-                this.$refs.cancel.addEventListener('click',async ()=>{
-                    this.$emit('close')
-                    resolve(false)
-                },true)
-                overy.addEventListener('click',async ()=>{
-                    this.$emit('close')
-                    resolve(false)
-                },true)
-                closeIcon.addEventListener('click',async ()=>{
-                    this.$emit('close')
-                    resolve(false)
-                },true)
-            })
-        },
-        //获取裁剪文件信息
-        getCropBlob(){
-            let {outputType}=this
-            return new Promise(resolve => {
-                this.$refs.cropper.getCropBlob(data=>{
-                    let file = new File([data], `cropper.${outputType.split('/')[1]}`,{type: outputType, lastModified: Date.now()})
-                    this.$emit('cropperSuccess',file)
-                    resolve(file)
-                })
-            })
-        },
-        imgLoad(){
-            //console.log('imgLoad')
-            this.showLoading=false
-        },
+      // 放大/缩小
+      changeScale(num) {
+        num = num || 1
+        this.$refs.cropper.changeScale(num)
+      },
+      // 旋转
+      rotate(type) {
+        //type 0 左旋 1右旋
+        type ? this.$refs.cropper.rotateRight() : this.$refs.cropper.rotateLeft()
+      },
+      // 实时预览函数
+      realTime(data) {
+        // //console.log('realTime')
+        // //console.log(data)
+        this.previews = data
+      },
+      //绑定监听函数
+      openDialog(){
+        this.showLoading=true
+        // //console.log(this.$refs.customDialog)
+        let {overy,closeIcon}=this.$refs.customDialog.$refs
+        // //console.log(overy)
+        // //console.log(closeIcon)
+        return new Promise((resolve,reject)=>{
+          this.$refs.complete.addEventListener('click',async ()=>{
+            let file=await this.getCropBlob()
+            this.$emit('cropperSuccess',file)
+            resolve(file)
+          },true)
+          this.$refs.cancel.addEventListener('click',async ()=>{
+            this.$emit('close')
+            resolve(false)
+          },true)
+          overy.addEventListener('click',async ()=>{
+            this.$emit('close')
+            resolve(false)
+          },true)
+          closeIcon.addEventListener('click',async ()=>{
+            this.$emit('close')
+            resolve(false)
+          },true)
+        })
+      },
+      //获取裁剪文件信息
+      getCropBlob(){
+        let {outputType}=this
+        return new Promise(resolve => {
+          this.$refs.cropper.getCropBlob(data=>{
+            let file = new File([data], `cropper.${outputType.split('/')[1]}`,{type: outputType, lastModified: Date.now()})
+            this.$emit('cropperSuccess',file)
+            resolve(file)
+          })
+        })
+      },
+      imgLoad(){
+        //console.log('imgLoad')
+        this.showLoading=false
+      },
     }
-}
+  }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 
 const path = require('path')
 function resolve(dir){
-    return path.join(__dirname,dir)//path.join(__dirname)设置绝对路径
+  return path.join(__dirname,dir)//path.join(__dirname)设置绝对路径
 }
 module.exports = {
-    chainWebpack:config=>{
-        config.module.rule('js')
-            .include.add(resolve('packages')).end()
-            .use('babel')
-            .loader('babel-loader')
-            .tap(options=>{
-                return options
-            })
-    },
-    // // 生产环境是否生成 sourceMap 文件
-    productionSourceMap: false,
-    parallel: require('os').cpus().length > 1,
+  chainWebpack:config=>{
+    config.module.rule('js')
+      .include.add(resolve('packages')).end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options=>{
+        return options
+      })
+  },
+  // // 生产环境是否生成 sourceMap 文件
+  productionSourceMap: false,
+  parallel: require('os').cpus().length > 1,
 }
