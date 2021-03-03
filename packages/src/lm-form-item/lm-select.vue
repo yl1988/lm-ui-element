4f4e4fd090d057f01523571213ef26a4c17e8d7a
杨涟 <yanglian@7build.com>
2021-03-03 17:18:20 +0800

    去掉lm-select v-on v-bind

--- packages/src/lm-form-item/lm-select.vue
+++ packages/src/lm-form-item/lm-select.vue
@@ -11,8 +11,6 @@
                            :style="{width:lmFormItemWidth}"
                            :filterable="filterable" :disabled="disabled"
                            :id="lmRef[0]" clearable
-                           v-bind="$attrs"
-                           v-on="$listeners"
                 >
                     <el-option :label="o[oName] || o" :value="o[oValue] || index" v-for="(o,index) in list" :key="index"></el-option>
                 </el-select>
