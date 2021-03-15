6207e3b7c09c9f5a8546396917b7dab3c50b52bd
杨涟 <yanglian@7build.com>
2021-03-15 10:35:14 +0800

    地址组件修改，弹窗组件控制最大高度

--- packages/src/lm-address/lm-address.vue
+++ packages/src/lm-address/lm-address.vue
@@ -221,7 +221,9 @@ export default {
       this.$emit('input', this.address)
       this.$emit('addressChange',this.address)
       if(value || this.addressArea.length){
-        let searchValue=this.addressArea.join('')+(value || '')
+        let searchAddressArea=JSON.parse(JSON.stringify(this.addressArea))
+        ;(searchAddressArea[1]===searchAddressArea[2]) && (searchAddressArea.splice(2,1))
+        let searchValue=searchAddressArea.join('')+(value || '')
         let addressInfos=await this.getSearchAddresList(searchValue)
         this.inputQueryData=addressInfos instanceof Array ? addressInfos.reduce((result,current)=>{
           let {name,address,location={}}=current
